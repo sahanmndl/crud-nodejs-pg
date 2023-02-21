03f5dca747dc74a53d9e5eebebf2ec811ac9dcc4
sahanmndl <mondalsahan053@gmail.com>
2023-02-21 13:05:26 +0530

    Updated

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,11 +1,11 @@
 import pool from "../index.js";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
 export const register = async (req, res, next) => {
     try {
         const { email, password, name } = req.body
-        const hash = await bcrypt.hash(password, 10)
+        const hash = bcrypt.hashSync(password, 10)
         const result = await pool.query('INSERT INTO users (email, password, name) VALUES ($1, $2, $3) RETURNING id', [email, hash, name])
         const user = { id: result.rows[0].id, email: email, name: name }
         //const token = jwt.sign({ user }, 'secret_key')
@@ -21,7 +21,7 @@ export const login = async (req, res, next) => {
         const { email, password } = req.body
         const result = await pool.query('SELECT * FROM users WHERE email = $1', [email])
         const user = result.rows[0]
-        if (user && await bcrypt.compare(password, user.password)) {
+        if (user && bcrypt.compareSync(password, user.password)) {
             //const token = jwt.sign({ user: { id: user.id, email: user.email } }, 'secret_key');
             res.status(201).json({ user })
         } else {
